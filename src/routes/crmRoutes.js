a72e8935e8c98a2a322eae7a7143f77846f74db1
Mauricio Robayo <rfmajo@gmail.com>
2020-11-13 21:37:50 -0500

    Initial commit

---
+++ src/routes/crmRoutes.js
@@ -0,0 +1,42 @@
+import { 
+    addNewContact, 
+    getContacts, 
+    getContactWithID, 
+    updateContact,
+    deleteContact 
+} from '../controllers/crmController';
+import { login, loginRequired, register } from '../controllers/userController';
+
+const routes = (app) => {
+    app.route('/contacts')
+    .get((req, res, next) => {
+        // middleware
+        console.log(`Request from: ${req.originalUrl}`)
+        console.log(`Request type: ${req.method}`)
+        next();
+    }, loginRequired, getContacts)
+    
+    // POST endpoint
+    .post(loginRequired, addNewContact);
+
+    app.route('/contact/:contactId')
+    // get specific contact
+    .get(loginRequired, getContactWithID)
+    
+    // put request
+    .put(loginRequired, updateContact)
+
+    // delete request
+    .delete(loginRequired, deleteContact);
+
+    app.route('/auth/register')
+    .post((req, res, next)=>{
+        console.log('hellos');
+        next()
+    }, register)
+    
+    app.route('/login')
+    .post(login)
+}
+
+export default routes;
